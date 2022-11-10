bb78f0995872b2b9dd1456f4e0c6c904fd38718b
Siarhei <sdergynov@gmail.com>
2022-11-10 11:35:54 +0300

    crete module rest

---
+++ rest-service/src/test/java/com/epam/dzerhunou/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.dzerhunou.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
